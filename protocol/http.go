158d915c2726822742dc4d41553a27aa254458a0
luyasanr <luyasanr@gmail.com>
2023-09-08 15:38:04 +0800

    hello world

--- protocol/http.go
+++ protocol/http.go
@@ -1,7 +1,9 @@
 package protocol
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/luyasr/hello-world/config"
 	"github.com/luyasr/hello-world/protocol/router"
 	"log"
 )
@@ -11,5 +13,5 @@ func Start() {
 	r := gin.Default()
 	api := r.Group("apiV1")
 	router.UserRouterInit(api)
-	log.Fatal(r.Run())
+	log.Fatal(r.Run(fmt.Sprintf(":%d", config.Conf.Server.Port)))
 }
